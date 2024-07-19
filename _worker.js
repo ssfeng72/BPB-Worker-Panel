7d6cb08e2c5639478edee48e7209c3ea6c00fc69
ssfeng72 <123847225+ssfeng72@users.noreply.github.com>
2024-07-20 02:39:13 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = '2a989d30-3d79-4412-9682-88a5a3da1024';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
